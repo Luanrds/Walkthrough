800d970b3214023db502716a3071f844cda85ee6
Luan Rodrigues <luanrsilva209@gmail.com>
2025-10-15 21:10:25 -0300

    step 32 - Adição de botão de voltar uma página no arquivo

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History",
 	"sap/m/MessageToast"
-], (Controller, MessageToast) => {
+], (Controller, History, MessageToast) => {
 	"use strict";
 
 	return Controller.extend("ui5.walkthrough.controller.Detail", {
@@ -16,6 +17,18 @@ sap.ui.define([
 				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
 				model: "invoice"
 			});
+		},
+
+		onNavBack() {
+			const oHistory = History.getInstance();
+			const sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				const oRouter = this.getOwnerComponent().getRouter();
+				oRouter.navTo("overview", {}, true);
+			}
 		},
 
 		onRatingChange(oEvent) {
